a1edf67dacef1bb9419edb4a0317283e9878fcce
nkr2747 <nilesh.kumar.2747@gmail.com>
2025-09-09 23:05:50 +0530

    multicore cpu for FIFO created

--- LAB3/MultiCoreProcessor.cpp
+++ LAB3/MultiCoreProcessor.cpp
@@ -1,16 +1,17 @@
-#include<bitset>/stdc++.h>
-#include<queue>
-#include"MultiCoreProcessor.h"
-#include<fstream>
+#include <bitset>/stdc++.h>
+#include <queue>
+#include "MultiCoreProcessor.h"
+#include <fstream>
 using namespace std;
 
-void MultiCoreProcessor::FIFO(vector<Process*> parsed_data,char* process_file){
-    ofstream output;
-    string filename = process_file;
-    string timeline = "Schedule(FIFO)";
-    timeline += filename[7];
-    timeline += ".txt";
-    output.open(timeline);
+void MultiCoreProcessor::FIFO(vector<Process *> parsed_data, char *process_file)
+{
+    // ofstream output;
+    // string filename = process_file;
+    // string timeline = "Schedule(FIFO)";
+    // timeline += filename[7];
+    // timeline += ".txt";
+    // output.open(timeline);
     int time = parsed_data[0]->arrival;
     queue<Process *> ready;
     queue<Process *> waiting;
@@ -21,12 +22,51 @@ void MultiCoreProcessor::FIFO(vector<Process*> parsed_data,char* process_file){
     int run1 = 0;
     int run2 = 0;
     int time = 0;
-    //output << "CPU0\n";
+    // output << "CPU0\n";
     while (!ready.empty() || !waiting.empty())
     {
-        if(run1 == 0){
-            
+        for(; i< parsed_data.size(); i++){
+            if(parsed_data[i]->arrival <= time){
+                ready.push(parsed_data[i]);
+            }
+            else break;
         }
+        while(!waiting.empty()){
+            Process* top = waiting.front();
+            int index = top->index;
+            int ioBurst = top->bursts[index];
+            int last = top->wait;
+            if(last + ioBurst <= time){
+                waiting.pop();
+                ready.push(top);
+            }
+            else{
+                break;
+            }
+        }
+        if (run1 == 0)
+        {
+            if (!ready.empty())
+            {
+                Process *top = ready.front();
+                ready.pop();
+                int index = top->index;
+                run1 = top->bursts[index];
+            }
+        }
+        if (run2 == 0)
+        {
+            if (!ready.empty())
+            {
+                Process *top = ready.front();
+                ready.pop();
+                int index = top->index;
+                run2 = top->bursts[index];
+            }
+        }
+        if(run1 > 0) run1--;
+        if(run2 > 0) run2--;
+        time++;
         // Process *cur = ready.front();
         // ready.pop();
         // int ind = cur->index;
@@ -77,8 +117,9 @@ void MultiCoreProcessor::FIFO(vector<Process*> parsed_data,char* process_file){
         //     }
         // }
     }
-    output << "Maximum TAT: " << maxTAT << endl;
-    output << "Average TAT: " << avgTAT / (float)parsed_data.size();
-    output.close();
+    cout<<time;
+    // output << "Maximum TAT: " << maxTAT << endl;
+    // output << "Average TAT: " << avgTAT / (float)parsed_data.size();
+    // output.close();
     return;
 }
--- LAB3/Processor.h
+++ LAB3/Processor.h
@@ -12,6 +12,13 @@ class Processor{
     void RR(vector<Process*> parsed_data,char* filename,int timeInterval);
 };
 
+class MultiCoreProcessor{
+    public:
+    void FIFO(vector<Process*> parsed_data,char* filename);
+    void NPSJF(vector<Process*> parsed_data,char* filename);
+    void PSJF(vector<Process*> parsed_data,char* filename);
+    void RR(vector<Process*> parsed_data,char* filename,int timeInterval);
+};
 // class FIFO{
 //     public:
 //     void run(vector<Process*> parsed_data,char* filename);
